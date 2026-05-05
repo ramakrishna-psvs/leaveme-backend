8d724860300b8febf399e580f4036334a4f8e851
RamakrishnaPinjala <heyofreak19@gmail.com>
2026-05-05 11:25:00 +1000

    fix: create users table on startup

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "LeaveMe API running 🚀"}
-
-
-from fastapi import FastAPI
 from app.routes import auth
+from app.database import Base, engine
+from app.models.user import User  
 
 app = FastAPI()
-
-app.include_router(auth.router, prefix="/auth")
-
-from app.database import Base, engine
-from app.models.user import User
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router, prefix="/auth")
